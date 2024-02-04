a35dea1b7f31e248d57c9e64f013fd2aa13e0d10
belluki <belluk.kim@gmail.com>
2024-02-04 22:34:48 +0900

    bgImg

--- js/background.js
+++ js/background.js
@@ -1,10 +1,14 @@
 const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg"];
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
 const bgImage = document.createElement("img");
+const changeBtn = document.querySelector("#nextImg");
 
-bgImage.src = `img/${chosenImage}`;
-bgImage.classList.add("bgimage");
-
-document.body.prepend(bgImage);
+function imgChange() {
+  const rnImg = () => Math.floor(Math.random() * images.length);
+  const chosenImage = images[rnImg()];
+  bgImage.src = `img/${chosenImage}`;
+  bgImage.classList.add("bgimage");
+  document.body.prepend(bgImage);
+}
+imgChange();
+changeBtn.addEventListener("click", imgChange);
